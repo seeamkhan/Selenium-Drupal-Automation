22df6c14a61e2d3504c09a365a94d06c80c8bfc2
seeam khan <zafi005@gmail.com>
2016-08-29 17:41:29 +0600

    some new

--- pen.py
+++ pen.py
@@ -4,14 +4,16 @@ import itertools
 
 
 def minion(str):
-    person_a_name = 'Stuart'
-    person_b_name = 'Kevin'
+    person_a_name = 'Vowels'
+    person_b_name = 'Consonants'
     vowels = ['A','E','I','O','U']
     consonants = ['Q','W','R','T','Y','P','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
     char_list = list(str)
     # print (char_list)
     total_char = len(char_list)
     # print (total_char)
+    person_a_words = []
+    person_b_words = []
 
     all_words = []
     for count in xrange(total_char):
@@ -23,6 +25,10 @@ def minion(str):
             # print temp_array
             all_words.append(temp_array)
     print all_words
+
+    for array in all_words:
+        if array[0] in vowels:
+            person_a_words.append(array)
 
 
 
